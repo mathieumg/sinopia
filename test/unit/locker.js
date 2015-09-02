e1a9346939d0a8419e49b2e255413c7be851c688
James Treworgy <jamietre@gmail.com>
2015-09-02 14:54:25 -0400

    Add timeout to resource lock

--- test/unit/locker.js
+++ test/unit/locker.js
@@ -1,9 +1,9 @@
 var assert   = require('assert')
-var lockerF = require('../../lib/utils').locker;
+var Locker = require('../../lib/locker');
 
 describe('Locker', function() {
   it('calls the original callback', function(done) {
-    var locker = lockerF();
+    var locker = new Locker();
 
     var called = false;
 
@@ -18,11 +18,9 @@ describe('Locker', function() {
       done();
     });
   })
-})
 
-describe('Locker', function() {
   it('read locks do not block other read locks', function(done) {
-    var locker = lockerF();
+    var locker = new Locker();
 
     var locks = 0;
 
@@ -46,12 +44,9 @@ describe('Locker', function() {
     unlock[0]();
     unlock[1]();
   })
-})
 
-
-describe('Locker', function() {
   it('write locks block all locks', function(done) {
-    var locker = lockerF();
+    var locker = new Locker();
 
     var locks = 0;
 
@@ -84,5 +79,57 @@ describe('Locker', function() {
     }));
 
     setImmediate(unlocks[0]);
+  })
+
+  it('releases locks when a new lock is requested after timeout expires', function(done) {
+    var locker = new Locker({
+      timeout: '200ms'
+    })
+
+    function cb(err) {
+      assert.ok(err && err.message.match(/Operation took longer/))
+    }
+
+    // this lock should be released after 200 ms
+    locker.writeLock(1, cb, function() {})
+
+    // this lock should be called when prior lock is released -- a timer will be created to release it
+    locker.writeLock(1, null, function(unlock) {
+      assert.ok(true, "pending lock was obtained after 1st failed")
+      unlock()
+    })
+
+    // the final lock will be added only after the others have been released automatically
+    setTimeout(function() {
+      locker.writeLock(1, null, function(unlock) {
+        assert.ok(true, 'lock was released');
+        unlock()
+        done()
+      })
+    }, 600)
+  })
+
+  it ('releases expired locks only when another lock is obtained', function(done) {
+    var locker = new Locker({
+      timeout: '200ms'
+    })
+
+    var released = false;
+
+    function cb(err) {
+      assert.ok(err && err.message.match(/Operation took longer/))
+      released = true
+    }
+
+    locker.readLock(1, cb, function() {})
+
+    setTimeout(function() {
+      assert.ok(!released)
+      locker.readLock(1, null, function(unlock) {
+        assert.ok(released)
+        unlock()
+        done()
+      })
+    }, 400)
   })
 });
