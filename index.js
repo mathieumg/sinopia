cd16f9fb18ebe1937e580ad148bd164689ebfc6c
James Treworgy <jamietre@gmail.com>
2015-10-02 13:10:05 -0400

    Fix regression issue in search API test newnmpreg /-/all (new version of express??)'

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ module.exports = require('./lib')
 
 /**package
 { "name": "sinopia",
-  "version": "0.0.0",
-  "dependencies": {"js-yaml": "*"},
-  "scripts": {"postinstall": "js-yaml package.yaml > package.json ; npm install"}
+  "version": "0.0.0"
 }
 **/
